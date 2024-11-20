561209580c7c4ec5795b5842b0ba8f7423bda8ae
akihito7 <akihitopro7@gmail.com>
2024-11-20 00:22:19 -0300

    get metrics revenue by month

--- src/app/orders/orders.controller.ts
+++ src/app/orders/orders.controller.ts
@@ -19,4 +19,9 @@ export class OrdersController {
     if(!date) throw new BadRequestException("Please provide a day in the request query! Example day: 19/11/2024");
     return this.ordersService.fetchCountOrdersByMonth(date);
   }
+
+  @Get("metrics-revenue-by-month")
+  async getTotalRevenueByMonth(){
+    return this.ordersService.fetchMetricsRevenueByMonth();
+  }
 }
--- src/app/orders/orders.service.ts
+++ src/app/orders/orders.service.ts
@@ -6,23 +6,9 @@ export class OrdersService {
   constructor(private readonly prisma: PrismaService) {}
 
   async fetchCountOrdersByDay(day: string) {
-    const startDateFormatted = new Date(day + ' 00:00:00')
-    const endDateFormatted = new Date(day + ' 23:59:59')
- 
-    return this.prisma.orders.count({
-      where : {
-        order_date: {
-          gte: startDateFormatted,
-          lte: endDateFormatted, 
-        },
-      }
-    })
-  }
+    const startDateFormatted = new Date(day + ' 00:00:00');
+    const endDateFormatted = new Date(day + ' 23:59:59');
 
-  async fetchCountOrdersByMonth(date: string) {
-    const inputDate = new Date(date);
-    const startDateFormatted = new Date(inputDate.getFullYear(), inputDate.getMonth(), 1, 0, 0, 0, 0);
-    const endDateFormatted = new Date(inputDate.getFullYear(), inputDate.getMonth() + 1, 0, 0, 59, 59, 999);
     return this.prisma.orders.count({
       where: {
         order_date: {
@@ -32,4 +18,97 @@ export class OrdersService {
       },
     });
   }
+
+  async fetchCountOrdersByMonth(date: string) {
+    const inputDate = new Date(date);
+    const startDateFormatted = new Date(
+      inputDate.getFullYear(),
+      inputDate.getMonth(),
+      1,
+      0,
+      0,
+      0,
+      0,
+    );
+    const endDateFormatted = new Date(
+      inputDate.getFullYear(),
+      inputDate.getMonth() + 1,
+      0,
+      0,
+      59,
+      59,
+      999,
+    );
+    return this.prisma.orders.count({
+      where: {
+        order_date: {
+          gte: startDateFormatted,
+          lte: endDateFormatted,
+        },
+      },
+    });
+  }
+
+  async fetchMetricsRevenueByMonth() {
+    const startDateCurrentMonth = new Date();
+    startDateCurrentMonth.setDate(1);
+    startDateCurrentMonth.setHours(0, 0, 0, 0);
+  
+    const endDateCurrentMonth = new Date(startDateCurrentMonth);
+    endDateCurrentMonth.setMonth(startDateCurrentMonth.getMonth() + 1);
+    endDateCurrentMonth.setDate(0);
+    endDateCurrentMonth.setHours(0, 59, 59, 999);
+ 
+    const currentMonthRevenue = await this.prisma.orders.aggregate({
+      _sum: {
+        total_price: true,
+      },
+      where: {
+        order_date: {
+          gte: startDateCurrentMonth,
+          lte: endDateCurrentMonth,
+        },
+      },
+    });
+  
+    const currentMonthTotalRevenue = currentMonthRevenue._sum.total_price || 0;
+
+    const startDatePreviousMonth = new Date(startDateCurrentMonth);
+    startDatePreviousMonth.setMonth(startDateCurrentMonth.getMonth() - 1);
+    startDatePreviousMonth.setDate(1);
+    startDatePreviousMonth.setHours(0, 0, 0, 0);
+
+  
+    const endDatePreviousMonth = new Date(startDatePreviousMonth);
+    endDatePreviousMonth.setMonth(startDatePreviousMonth.getMonth() + 1);
+    endDatePreviousMonth.setDate(0);
+    endDatePreviousMonth.setHours(0, 59, 59, 999);
+
+  
+    const previousMonthRevenue = await this.prisma.orders.aggregate({
+      _sum: {
+        total_price: true,
+      },
+      where: {
+        order_date: {
+          gte: startDatePreviousMonth,
+          lte: endDatePreviousMonth,
+        },
+      },
+    });
+  
+    const previousMonthTotalRevenue = previousMonthRevenue._sum.total_price || 0;
+
+    let percentageChange = 0;
+    if (Number(previousMonthTotalRevenue) > 0) {
+      percentageChange = Number((((Number(currentMonthTotalRevenue) - Number(previousMonthTotalRevenue)) / Number(previousMonthTotalRevenue)) * 100).toFixed(2));
+    }
+  
+    return {
+      currentMonthTotalRevenue,
+      /* previousMonthTotalRevenue, */
+      percentageChange,
+    };
+  }
+
 }
