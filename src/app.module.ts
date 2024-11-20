8bc45de1a0f4fc4525a5f4ed13457bcd6402cfd7
akihito7 <akihitopro7@gmail.com>
2024-11-19 22:08:02 -0300

    modules structure initial

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthenticationModule } from './app/authentication/authentication.module';
+import { EmployeesModule } from './app/employees/employees.module';
+import { OrdersModule } from './app/orders/orders.module';
 
 @Module({
-  imports: [],
+  imports: [AuthenticationModule, EmployeesModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3333);
+  const PORT = 3333;
+  await app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
 }
 bootstrap();
